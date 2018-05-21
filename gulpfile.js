bb25e8858ff99b436a48ed0eb1770ea20e328143
Andrey <xferra@live.com>
2018-05-21 11:52:36 +0300

    Build process for versioned schemas

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,43 @@
-var gulp = require('gulp');
+var path = require('path'),
+    gulp = require('gulp'),
+    subdirRename = require('gulp-subdir-rename'),
+    jsonFormat = require('gulp-json-format');
+
+gulp.task('format', function() {
+    var stream = gulp
+        .src('src/*.json')
+        .pipe(jsonFormat(4))
+        .pipe(gulp.dest('src'));
+    return stream;
+});
+
+gulp.task('build', [ 'format' ], function() {
+    var stream = gulp
+        .src('src/*.json')
+        .pipe(jsonFormat(4))
+        .pipe(gulp.dest('build'));
+    return stream;
+});
 
 gulp.task('static', function() {
     var stream = gulp
         .src('CNAME')
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest('dist'));
     return stream;
-})
+});
 
-gulp.task('build', function() {
+gulp.task('release', [ 'static', 'build' ], function() {
+    var version
     var stream = gulp
-        .src('src/*.json')
-        .pipe(gulp.dest('build'));
-    return stream;
-})
+        .src('build/*.json')
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(function(file) {
+            var content = JSON.parse(file.contents.toString());
+            var version = content.$version;
+            return path.join('dist', version);
+        }));
 
-gulp.task('default', ['static', 'build']);
+    return stream;
+});
+
+gulp.task('default', [ 'build' ]);
