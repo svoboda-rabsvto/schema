bd4ff721a76da538f9159645c65f7978c9f79591
Andrey <xferra@live.com>
2018-05-25 17:54:46 +0300

    Finish gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// External modules
 var path = require('path'),
     fs = require("fs"),
     gulp = require('gulp'),
@@ -5,59 +6,92 @@ var path = require('path'),
     jsonSchema = require("gulp-json-schema"),
     jsonSchemaBundle = require('gulp-jsonschema-bundle');
 
+// Path and mask config
+var config = {
+    src: {
+        dir: 'src',
+        mask: 'src/**/*.json'
+    },
+    schema: {
+        mask: 'src/schema/*.json',
+        ver: 'src/schema/schemaver.json',
+        collection: 'src/schema/collection.json'
+    },
+    build: {
+        dir: 'build',
+        mask: 'build/*.json'
+    },
+    dist: {
+        dir: 'dist'
+    },
+    assets: {
+        cname: 'CNAME'
+    }
+};
+
+// Reformat and save all source schemas
 gulp.task('format', function() {
     var stream = gulp
-        .src('src/**/*.json')
+        .src(config.src.mask)
         .pipe(jsonFormat(4))
-        .pipe(gulp.dest('src'));
+        .pipe(gulp.dest(config.src.dir));
     return stream;
 });
 
+// Validate all core schemas according to schemaver
 gulp.task('validate', [ 'format' ], function() {
     var stream = gulp
-        .src('src/schema/*.json')
+        .src(config.schema.mask)
         .pipe(jsonSchema({
-            schema: 'src/schema/schemaver.json',
+            schema: config.schema.ver,
             loadMissingSchemas: true,
             checkRecursive: true
         }));
     return stream;
 });
 
+// Resolve all $ref in collections and create a bundle
 gulp.task('bundle', [ 'validate' ], function() {
-    var stream = gulp.src('src/schema/collection.json')
+    var stream = gulp.src(config.schema.collection)
         .pipe(jsonSchemaBundle())
         .pipe(jsonFormat(4))
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(config.build.dir));
     return stream;
 });
 
+// Build all schema sources
 gulp.task('build', [ 'bundle' ], function() {
     var stream = gulp
         .src([
-            'src/schema/*.json',
-            '!src/schema/collection.json'
+            config.schema.mask,
+            '!' + config.schema.collection
         ])
-        .pipe(gulp.dest('build'));
+        .pipe(gulp.dest(config.build.dir));
     return stream;
 });
 
+// Copy all static assets to release
 gulp.task('static', function() {
     var stream = gulp
-        .src('CNAME')
-        .pipe(gulp.dest('dist'));
+        .src(config.assets.cname)
+        .pipe(gulp.dest(config.dist.dir));
     return stream;
 });
 
-gulp.task('release', [ 'static', 'build' ], function() {
-    var version
+// Run tests over files in test folder
+gulp.task('test', function() {
+    // TODO: Implement (probably separate tasks for src, dist & build)
+});
+
+// Build and release (copy to dist) current version
+gulp.task('release', [ 'static', 'build', 'test' ], function() {
     var stream = gulp
-        .src('build/*.json')
-        .pipe(gulp.dest('dist'))
+        .src(config.build.mask)
+        .pipe(gulp.dest(config.dist.dir))
         .pipe(gulp.dest(function(file) {
             var content = JSON.parse(file.contents.toString());
             var version = content.$version;
-            return path.join('dist', version);
+            return path.join(config.dist.dir, version);
         }));
 
     return stream;
