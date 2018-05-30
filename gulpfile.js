972bf24c271ccf4f14a78aef97205d3baaf0c2ab
svoboda-rabstvo <romanrostislavovich@gmail.com>
2018-05-30 18:37:08 +0300

    add submodule and import licenses and languages

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,36 @@
+/* jshint esversion: 6 */
+
 // External modules
 var path = require('path'),
     fs = require("fs"),
     gulp = require('gulp'),
     jsonFormat = require('gulp-json-format'),
     jsonSchema = require("gulp-json-schema"),
+    jsonData = require('gulp-data');
     jsonSchemaBundle = require('gulp-jsonschema-bundle');
+    yaml = require('js-yaml');
+
 
 // Path and mask config
 var config = {
     src: {
         dir: 'src',
         mask: 'src/**/*.json'
+    },
+    template: {
+        linguist: "src/template/language.linguist.json",
+        spdx: "src/template/license.spdx.json"
+    },
+    collection: {
+        dir: "src/collection/",
+        licenses: {
+            linter: "src/collection/license.linter.json",
+            spdx: "src/collection/license.spdx.json"
+        },
+        languages: {
+            linter: "src/collection/language.linter.json",
+            linguist: "src/collection/language.linguist.json"
+        }
     },
     schema: {
         mask: 'src/schema/*.json',
@@ -20,6 +40,10 @@ var config = {
     build: {
         dir: 'build',
         mask: 'build/*.json'
+    },
+    ext: {
+        linguist: 'ext/github/linguist/lib/linguist/languages.yml',
+        spdx: 'ext/spdx/license-list-data/json/licenses.json'
     },
     dist: {
         dir: 'dist'
@@ -66,9 +90,9 @@ const static = () => gulp
     .pipe(gulp.dest(config.dist.dir));
 
 // Run tests over files in test folder
-const test = () => {
+const test = (done) => {
     // TODO: Implement (probably separate tasks for src, dist & build)
-    return;
+    done();
 };
 
 // Build and release (copy to dist) current version
@@ -81,6 +105,45 @@ const release = () => gulp
         return path.join(config.dist.dir, version);
     }));
 
+// Import languages of linguist to file 
+const importLanguages = () => gulp 
+    .src(config.template.linguist)
+    .pipe(jsonData((file) => {
+        const list = yaml.safeLoad(fs.readFileSync(config.ext.linguist));
+        const template = JSON.parse(fs.readFileSync(file.path));
+
+        template.definitions.language.oneOf = Object.keys(list).map(name =>
+        {
+            let item = list[name];
+            let language =
+            {
+                "enum": [name]
+            };
+            if (item.extensions && item.extensions.length) language.extensions = item.extensions;
+            if (item.filenames && item.filenames.length) language.filenames = item.filenames;
+            return language;
+        });
+
+        const formatted = JSON.stringify(template, null, 4);
+        file.contents = Buffer.from(formatted, 'utf8');
+        return file;
+    }))
+    .pipe(gulp.dest(config.collection.dir));
+
+// Import licenses of spdx to file 
+const importLicenses = () => gulp
+    .src(config.template.spdx)
+    .pipe(jsonData((file) => {
+        const list = JSON.parse(fs.readFileSync(config.ext.spdx));
+        const template = JSON.parse(fs.readFileSync(file.path));        
+        template.enum = list.licenses.map(l => l.licenseId);
+        
+        const formatted = JSON.stringify(template, null, 4);
+        file.contents = Buffer.from(formatted, 'utf8');
+        return file;
+    }))
+    .pipe(gulp.dest( config.collection.dir));   
+
 // Tasks
 gulp.task('format', format);
 gulp.task('validate', validate);
@@ -89,5 +152,6 @@ gulp.task('build', gulp.series('bundle', build));
 gulp.task('static', static);
 gulp.task('test', test);
 gulp.task('release', gulp.series('static', 'build', /*'test',*/ release));
-
 gulp.task('default', gulp.series('build'));
+gulp.task('import-licenses', importLicenses);
+gulp.task('import-languages', importLanguages);
